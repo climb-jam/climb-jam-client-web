69464d349c99049f93c0cbf1241c7afbd8e1f94f
Alexandre <alexandre.delsol@hotmail.fr>
2025-09-10 11:40:22 +0200

    JAM-48 refacto crag api + add fetchCragById

---
+++ src/components/crags/CragDetails.tsx
@@ -0,0 +1,14 @@
+import Pages from "../../components/layout/Pages";
+
+const CragDetails = ({}) => {
+
+    return (
+        <Pages title={"Le spot"}>
+        <div>
+
+        </div>
+        </Pages>
+    );
+};
+
+export default CragDetails;
