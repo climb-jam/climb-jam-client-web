9aacc4173f3e0f97b25d6e02cb567f561e1793e3
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2026-01-04 15:55:14 +0100

    JAM-59 Created guest header and footer for pages where users not logged in + Changed theme context to include MUI theme for dark mode & Set up dark mode for pages without login

--- src/layout/LayoutWithoutNav.tsx
+++ src/layout/LayoutWithoutNav.tsx
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router";
+import GuestFooter from "../pages/A_nav/GuestFooter.tsx";
+import GuestHeader from "../pages/A_nav/GuestHeader.tsx";
 
 const LayoutWithoutNav = () => {
     return (
         <>
+            <GuestHeader/>
             <Outlet/>
+            <GuestFooter/>
         </>
     );
 };
